5998df3ea39b7e4c8b086b7bd339f6cedac66f61
reillyc <35342845+reillyc@users.noreply.github.com>
2019-02-07 12:24:36 -0800

    Add files via upload

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,15 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {
+	var friendName =  request.query.name;
+	var friendDesc =  request.query.description;
+	var newFriend = {
+		"name": friendName,
+		"description": friendDesc,
+		"imageURL": 'http://lorempixel.com/400/400/people',
+	};
+	
+	data.friends.push(newFriend);
+		console.log(newFriend);
+	response.render("index.handlebars", data);
+};
